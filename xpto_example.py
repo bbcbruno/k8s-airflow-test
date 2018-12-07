bd60ae1b7f0dbb67f2b8469da58084ef8b9b6c16
Bruno Campos <bruno@brunocampos.me>
2018-12-07 11:21:46 +0100

    test

---
+++ xpto_example.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.utcnow(),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'xpto_k8s', default_args=default_args, schedule_interval=timedelta(minutes=5))
+
+passing = KubernetesPodOperator(namespace='default',
+                          image="Python:3.6",
+                          cmds=["Python","-c"],
+                          arguments=["print('hello world')"],
+                          labels={"foo": "bar"},
+                          name="xpto",
+                          task_id="xpto",
+                          get_logs=True,
+                          dag=dag
+                          )
+
